74c52d1df0fa30860876c8f549e57b621224c37d
Ossany Insua Morfa <ossany.insua1982@gmail.com>
2021-03-03 13:44:29 -0500

    master

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import Link from './src/components/Linking/Linking';
+import Font from './src/components/FontFamily/Font';
+
 
 export default function App() {
+   
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Link/>
+      <Font />
       <StatusBar style="auto" />
     </View>
   );
